ae2438605cb052e839fcdf433701ee4f8c8d9858
Ian Duncan <76043277+dr8co@users.noreply.github.com>
2025-05-18 16:55:57 +0300

    Implement string concatenation

--- evaluator/evaluator.go
+++ evaluator/evaluator.go
@@ -208,6 +208,8 @@ func evalInfixExpression(operator string, left, right object.Object) object.Obje
 	case left.Type() != right.Type():
 		return newError("type mismatch: %s %s %s",
 			left.Type(), operator, right.Type())
+	case left.Type() == object.STRING_OBJ && right.Type() == object.STRING_OBJ:
+		return evalStringInfixExpression(operator, left, right)
 	default:
 		return newError("unknown operator: %s %s %s",
 			left.Type(), operator, right.Type())
@@ -294,4 +296,15 @@ func nativeBoolToBooleanObject(b bool) *object.Boolean {
 		return TRUE
 	}
 	return FALSE
+}
+
+func evalStringInfixExpression(operator string, left, right object.Object) object.Object {
+	if operator != "+" {
+		return newError("unknown operator: %s %s %s",
+			left.Type(), operator, right.Type())
+	}
+	leftVal := left.(*object.String).Value
+	rightVal := right.(*object.String).Value
+
+	return &object.String{Value: leftVal + rightVal}
 }

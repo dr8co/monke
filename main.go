39ed982df36cbdbadace6b7f2bf6b41a5f37702f
Ian Duncan <76043277+dr8co@users.noreply.github.com>
2025-05-21 14:47:59 +0300

    Add CLI options for script execution, evaluation, and version
    Added command-line options to execute a file, evaluate an expression, and display version information. Included support for disabling color output and enabling debug mode. Updated REPL initialization to handle new options and added helper functions for enhanced functionality.

--- main.go
+++ main.go
@@ -1,16 +1,135 @@
 package main
 
 import (
+	"flag"
+	"fmt"
+	"github.com/dr8co/monke/evaluator"
+	"github.com/dr8co/monke/lexer"
+	"github.com/dr8co/monke/object"
+	"github.com/dr8co/monke/parser"
 	"github.com/dr8co/monke/repl"
+	"io/ioutil"
+	"os"
 	"os/user"
 )
 
-func main() {
-	usr, err := user.Current()
+const VERSION = "0.1.0"
 
+func main() {
+	// Define command-line flags
+	noColor := flag.Bool("no-color", false, "Disable syntax highlighting and colored output")
+	fileFlag := flag.String("file", "", "Execute a Monkey script file")
+	evalFlag := flag.String("eval", "", "Evaluate a Monkey expression and print the result")
+	debugFlag := flag.Bool("debug", false, "Enable debug mode with more verbose output")
+	versionFlag := flag.Bool("version", false, "Show version information")
+
+	// Define short flag aliases
+	flag.BoolVar(noColor, "n", false, "Disable syntax highlighting and colored output")
+	flag.StringVar(fileFlag, "f", "", "Execute a Monkey script file")
+	flag.StringVar(evalFlag, "e", "", "Evaluate a Monkey expression and print the result")
+	flag.BoolVar(debugFlag, "d", false, "Enable debug mode with more verbose output")
+	flag.BoolVar(versionFlag, "v", false, "Show version information")
+
+	// Parse command-line flags
+	flag.Parse()
+
+	// Show version information if requested
+	if *versionFlag {
+		fmt.Printf("Monkey Programming Language v%s\n", VERSION)
+		return
+	}
+
+	// Get current user
+	usr, err := user.Current()
 	if err != nil {
 		panic(err)
 	}
 
-	repl.Start(usr.Username)
+	// Create options struct for REPL
+	options := repl.Options{
+		NoColor: *noColor,
+		Debug:   *debugFlag,
+	}
+
+	// Execute a file if specified
+	if *fileFlag != "" {
+		executeFile(*fileFlag, *debugFlag)
+		return
+	}
+
+	// Evaluate an expression if specified
+	if *evalFlag != "" {
+		evaluateExpression(*evalFlag, *debugFlag)
+		return
+	}
+
+	// Start the REPL
+	repl.Start(usr.Username, options)
+}
+
+// executeFile reads and executes a Monkey script file
+func executeFile(filename string, debug bool) {
+	// Read the file
+	content, err := ioutil.ReadFile(filename)
+	if err != nil {
+		fmt.Printf("Error reading file: %s\n", err)
+		os.Exit(1)
+	}
+
+	// Create environment
+	env := object.NewEnvironment()
+
+	// Parse and evaluate the file
+	l := lexer.New(string(content))
+	p := parser.New(l)
+	program := p.ParseProgram()
+
+	if len(p.Errors()) != 0 {
+		printParserErrors(p.Errors())
+		os.Exit(1)
+	}
+
+	evaluated := evaluator.Eval(program, env)
+
+	// Print the result if in debug mode
+	if debug && evaluated != nil {
+		fmt.Println(evaluated.Inspect())
+	}
+}
+
+// evaluateExpression evaluates a single Monkey expression
+func evaluateExpression(expr string, debug bool) {
+	// Create environment
+	env := object.NewEnvironment()
+
+	// Parse and evaluate the expression
+	l := lexer.New(expr)
+	p := parser.New(l)
+	program := p.ParseProgram()
+
+	if len(p.Errors()) != 0 {
+		printParserErrors(p.Errors())
+		os.Exit(1)
+	}
+
+	evaluated := evaluator.Eval(program, env)
+
+	// Print the result
+	if evaluated != nil {
+		fmt.Println(evaluated.Inspect())
+	}
+}
+
+// printParserErrors prints parser errors to stderr
+func printParserErrors(errors []string) {
+	_, err := fmt.Fprintln(os.Stderr, "Parser errors:")
+	if err != nil {
+		panic(err)
+	}
+	for _, msg := range errors {
+		_, err := fmt.Fprintln(os.Stderr, "\t"+msg)
+		if err != nil {
+			panic(err)
+		}
+	}
 }

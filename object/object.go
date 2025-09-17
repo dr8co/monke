76518ad35c00dfbfea37974661b53327d1f95c3c
Ian Duncan <76043277+dr8co@users.noreply.github.com>
2025-09-17 12:21:15 +0300

    Add comments to Monke objects and methods; minor cleanup

--- object/object.go
+++ object/object.go
@@ -18,11 +18,13 @@ package object
 import (
 	"fmt"
 	"hash/fnv"
+	"strconv"
 	"strings"
 
 	"github.com/dr8co/monke/ast"
 )
 
+//nolint:revive
 const (
 	INTEGER_OBJ      = "INTEGER"
 	BOOLEAN_OBJ      = "BOOLEAN"
@@ -36,65 +38,96 @@ const (
 	HASH_OBJ         = "HASH"
 )
 
+// ObjectType represents the type of object.
 type ObjectType string
 
+// Object is the interface that wraps the basic operations of all Monke objects.
+// All Monke objects implement this interface.
 type Object interface {
 	Type() ObjectType
 	Inspect() string
 }
 
+// Integer represents a Monke integer value.
 type Integer struct {
 	Value int64
 }
 
+// Type returns the type of the object.
 func (i *Integer) Type() ObjectType { return INTEGER_OBJ }
-func (i *Integer) Inspect() string  { return fmt.Sprintf("%d", i.Value) }
 
+// Inspect returns a string representation of the object.
+func (i *Integer) Inspect() string { return strconv.FormatInt(i.Value, 10) }
+
+// Boolean represents a Monke boolean value.
 type Boolean struct {
 	Value bool
 }
 
+// Type returns the type of the object.
 func (b *Boolean) Type() ObjectType { return BOOLEAN_OBJ }
-func (b *Boolean) Inspect() string  { return fmt.Sprintf("%t", b.Value) }
 
+// Inspect returns a string representation of the object.
+func (b *Boolean) Inspect() string { return strconv.FormatBool(b.Value) }
+
+// String represents a Monke string value.
 type String struct {
 	Value string
 	// Cache for the hash key to avoid recalculating it
 	hashKey *HashKey
 }
 
+// Type returns the type of the object.
 func (s *String) Type() ObjectType { return STRING_OBJ }
-func (s *String) Inspect() string  { return s.Value }
 
+// Inspect returns a string representation of the object.
+func (s *String) Inspect() string { return s.Value }
+
+// Null represents a Monke null value.
 type Null struct{}
 
+// Type returns the type of the object.
 func (n *Null) Type() ObjectType { return NULL_OBJ }
-func (n *Null) Inspect() string  { return "null" }
 
+// Inspect returns a string representation of the object.
+func (n *Null) Inspect() string { return "null" }
+
+// ReturnValue represents a Monke return value.
 type ReturnValue struct {
 	Value Object
 }
 
+// Type returns the type of the object.
 func (rv *ReturnValue) Type() ObjectType { return RETURN_VALUE_OBJ }
-func (rv *ReturnValue) Inspect() string  { return rv.Value.Inspect() }
 
+// Inspect returns a string representation of the object.
+func (rv *ReturnValue) Inspect() string { return rv.Value.Inspect() }
+
+// Error represents a Monke error.
 type Error struct {
 	Message string
 }
 
+// Type returns the type of the object.
 func (e *Error) Type() ObjectType { return ERROR_OBJ }
-func (e *Error) Inspect() string  { return "ERROR: " + e.Message }
 
+// Inspect returns a string representation of the object.
+func (e *Error) Inspect() string { return "ERROR: " + e.Message }
+
+// Function represents a Monke function.
 type Function struct {
 	Parameters []*ast.Identifier
 	Body       *ast.BlockStatement
 	Env        *Environment
 }
 
+// Type returns the type of the object.
 func (f *Function) Type() ObjectType { return FUNCTION_OBJ }
+
+// Inspect returns a string representation of the object.
 func (f *Function) Inspect() string {
 	var out strings.Builder
-	var params []string
+	params := make([]string, 0, len(f.Parameters))
 
 	for _, p := range f.Parameters {
 		params = append(params, p.String())
@@ -110,20 +143,29 @@ func (f *Function) Inspect() string {
 	return out.String()
 }
 
+// BuiltinFunction represents a Monke builtin function.
 type BuiltinFunction func(args ...Object) Object
 
+// Builtin represents a Monke builtin.
 type Builtin struct {
 	Fn BuiltinFunction
 }
 
+// Type returns the type of the object.
 func (b *Builtin) Type() ObjectType { return BUILTIN_OBJ }
-func (b *Builtin) Inspect() string  { return "builtin function" }
 
+// Inspect returns a string representation of the object.
+func (b *Builtin) Inspect() string { return "builtin function" }
+
+// Array represents a Monke array.
 type Array struct {
 	Elements []Object
 }
 
+// Type returns the type of the object.
 func (a *Array) Type() ObjectType { return ARRAY_OBJ }
+
+// Inspect returns a string representation of the object.
 func (a *Array) Inspect() string {
 	var out strings.Builder
 
@@ -139,11 +181,13 @@ func (a *Array) Inspect() string {
 	return out.String()
 }
 
+// HashKey represents a hash key.
 type HashKey struct {
 	Type  ObjectType
 	Value uint64
 }
 
+// HashKey returns the hash key for the object.
 func (b *Boolean) HashKey() HashKey {
 	var value uint64
 
@@ -155,10 +199,13 @@ func (b *Boolean) HashKey() HashKey {
 	return HashKey{Type: b.Type(), Value: value}
 }
 
+// HashKey returns the hash key for the object.
 func (i *Integer) HashKey() HashKey {
+	//nolint:gosec
 	return HashKey{Type: i.Type(), Value: uint64(i.Value)}
 }
 
+// HashKey returns the hash key for the object.
 func (s *String) HashKey() HashKey {
 	// Return the cached hash key if available
 	if s.hashKey != nil {
@@ -178,20 +225,25 @@ func (s *String) HashKey() HashKey {
 	return hashKey
 }
 
+// HashPair represents a hash pair.
 type HashPair struct {
 	Key   Object
 	Value Object
 }
 
+// Hash represents a Monke hash.
 type Hash struct {
 	Pairs map[HashKey]HashPair
 }
 
+// Type returns the type of the object.
 func (h *Hash) Type() ObjectType { return HASH_OBJ }
+
+// Inspect returns a string representation of the object.
 func (h *Hash) Inspect() string {
 	var out strings.Builder
 
-	var pairs []string
+	pairs := make([]string, 0, len(h.Pairs))
 	for _, pair := range h.Pairs {
 		pairs = append(pairs, fmt.Sprintf("%s: %s", pair.Key.Inspect(), pair.Value.Inspect()))
 	}
@@ -203,6 +255,7 @@ func (h *Hash) Inspect() string {
 	return out.String()
 }
 
+// Hashable represents an object that can be used as a hash key.
 type Hashable interface {
 	HashKey() HashKey
 }
